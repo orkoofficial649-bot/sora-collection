e31dc3d9f46f2d96b640852cd52f337f0316e119
orko barmon <orkobarmon81@gmail.com>
2026-04-20 02:56:21 +0600

    Fixed routing and index path

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Shob static file (HTML, CSS, Image) current folder theke load hobe
 app.use(express.static(path.join(__dirname, '.')));
 
-app.get('*', (req, res) => {
+// Main link-e (/) click korle index.html dekhabe
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Order link-e (/order) click korle order.html dekhabe
+app.get('/order', (req, res) => {
+    res.sendFile(path.join(__dirname, 'order.html'));
 });
 
 app.listen(PORT, () => {
